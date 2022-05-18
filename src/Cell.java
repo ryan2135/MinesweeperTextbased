0df17ebd1197e504d060c39dad786964c47b470a
Doug Ryan <ryan2135@pacificu.edu>
2022-05-18 15:42:39 -0700

    Initial design

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Cell {
+import java.util.Scanner;
+
+public class Cell {
+  short mID;
+  String mcName;
+  Boolean mcIsSelected;
+  String mcString;
+
+  public Cell () {
+    mID = 0;
+    mcName = "";
+    mcIsSelected = false;
+  }
+
+  public Cell (short id, String name, String string) {
+    mID = id;
+    mcName = name;
+    mcIsSelected = false;
+    mcString = string;
+  }
+
+  public Cell (Cell cCell) {
+    this.mID = cCell.mID;
+    this.mcName = cCell.mcName;
+    this.mcIsSelected = cCell.mcIsSelected;
+    this.mcString = cCell.mcString;
+  }
+
+  public void select () {
+    mcIsSelected = true;
+  }
+
+  public Boolean isSelected () {
+    return mcIsSelected;
+  }
+
+  /*
+  For now I"m going to implement a text-based draw cuz I don't know the
+  proper design for text, graphics, and sprite draw
+   */
+  public void draw () {
+    System.out.print (this.mcString);
+  }
 }
--- src/MinesweeperTextbased.java
+++ src/MinesweeperTextbased.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class MinesweeperTextbased {
-}
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MinesweeperTextbased {
+  public static final short NUM_ROWS = 9;
+  public static final short NUM_COLS = 9;
+  public static final short INITIAL_BOMBS = (short) Math.sqrt (NUM_COLS * NUM_ROWS);
+  public static final short ADDIONAL_BOMBS_PER_LEVEL = 3;
+
+
+  public static enum Difficulty {
+    EASY, MEDIUM, HARD
+  }
+
+  public static Difficulty checkDifficulty (Character difficulty) throws IOException {
+    Difficulty cDifficulty;
+
+    if (difficulty == 'E') {
+      cDifficulty = Difficulty.EASY;
+    }
+    else if (difficulty == 'M') {
+      cDifficulty = Difficulty.MEDIUM;
+    }
+    else if (difficulty == 'H') {
+      cDifficulty = Difficulty.HARD;
+    }
+    else {
+      throw new IOException ("Illegal Selection");
+    }
+    return cDifficulty;
+  }
+
+  public static void main (String[] args) {
+    Character difficultyLevel = 'E';
+    Difficulty cDifficulty;
+    // boolean bHitBomb;
+    short xCoord, yCoord;
+
+    Scanner input = new Scanner (System.in);
+
+    System.out.print ("***********\nMinesweeper\n***********\n\n");
+    System.out.print ("Enter difficulty level\n" +
+      "(E)ASY, M)EDIUM, H)ARD): ");
+
+    try {
+      difficultyLevel = input.next ().charAt (0);
+      cDifficulty = checkDifficulty (difficultyLevel);
+    }
+    catch (IOException difficultyException) {
+      System.out.println ("I/O error: " + difficultyException);
+    }
+  }
+/*
+
+            MineSweeper mMindSweeper = new MineSweeper (difficultyLevel, BOARD_SIZE);
+
+            do
+            {
+                System.out.println ();
+                mMindSweeper.printBoard ();
+                System.out.println ();
+                System.out.print ("Enter X and Y Coordinate: ");
+                xCoord = input.nextInt ();
+                yCoord = input.nextInt ();
+                bHitBomb = mMindSweeper.isBomb (xCoord, yCoord);
+                if (!bHitBomb)
+                {
+                    mMindSweeper.processBoard (xCoord, yCoord);
+                }
+                else
+                {
+                    System.out.println ("Boooom!!! You lose.");
+                }
+            } while (!bHitBomb);
+            input.close ();*/
+  }
